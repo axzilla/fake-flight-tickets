3d0984457512f08a614a39fdf43f4521af9d6f24
‘axzilla’ <chanter-acidic-0r@icloud.com>
2019-12-23 13:59:42 +0700

    Initial commit after splitting cliend and server into seperate repos

---
+++ nodemailer/templates/sendTicket.js
@@ -0,0 +1,25 @@
+const transporter = require('../transporter')
+
+module.exports = (pdf, email) => {
+  const mailOptions = {
+    from: 'noreply@fakeflighttickets.dev',
+    to: email,
+    subject: '#1 Fake Flight Ticket',
+    attachments: [
+      {
+        filename: 'flight_ticket.pdf',
+        content: pdf,
+        contentType: 'application/pdf'
+      }
+    ],
+    html: `
+        Hi ${email},<br><br>
+        you can find your fake flight ticket in the attachments.<br>
+        Thanks, #1 Fake Flight Ticket.<br><br>
+    `
+  }
+
+  transporter.sendMail(mailOptions, error => {
+    error ? console.log(error) : console.log(`Mail sent to ${email}`)
+  })
+}
