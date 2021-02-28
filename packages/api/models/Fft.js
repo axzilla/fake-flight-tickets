dcbed07d578c6e11db701f30141afe8342882a15
‘axzilla’ <chanter-acidic-0r@icloud.com>
2021-02-28 18:46:43 +0100

    Add date to model

--- packages/api/models/Fft.js
+++ packages/api/models/Fft.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 const FftSchema = new Schema({
   email: { type: String, required: true },
   flight: { type: Object, required: true },
-  passengers: { type: Object, required: true }
+  passengers: { type: Object, required: true },
+  dateCreated: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('Fft', FftSchema)
