440ccc617e68d317d163eb0788359ca2cf298935
‘axzilla’ <chanter-acidic-0r@icloud.com>
2021-03-01 15:48:14 +0100

    Update _Footer.js

--- packages/app/layout/_Footer.js
+++ packages/app/layout/_Footer.js
@@ -14,23 +14,6 @@ function Header() {
 
   return (
     <div className={classes.footer}>
-      <Grid container justify="center">
-        <Box mb={3}>
-          <a
-            href="https://www.producthunt.com/posts/fake-flight-tickets?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-fake-flight-tickets"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=286315&theme=light"
-              alt="Fake Flight Tickets - 'Real fake' onward or return flight tickets. | Product Hunt"
-              // style={"width: 250px; height: 54px;"}
-              width="250"
-              height="54"
-            />
-          </a>
-        </Box>
-      </Grid>
       <Typography align="center">
         &copy; {new Date().getFullYear()} FakeFlightTickets &#10084;&#65039;
       </Typography>
